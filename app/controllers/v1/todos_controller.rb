92c65e46c429357999025defaa203e952144f6ac
David Evhade <david@kobo360.com>
2020-04-05 18:22:27 +0100

    commit added files

---
+++ app/controllers/v1/todos_controller.rb
@@ -0,0 +1,53 @@
+module V1
+    class TodosController < ApplicationController
+        before_action :set_todo, only: [:show, :update, :destroy]
+        # GET /todos
+        def index
+            # @todos = Todo.all
+    
+            # get current user todos
+            @todos = current_user.todos
+            json_response(@todos)
+        end
+    
+        # POST /todo
+        def create
+            # @todo = Todo.create!(todo_params)
+    
+            # create todos belonging to current user
+            @todo = current_user.todos.create!(todo_params)
+            json_response(@todo, :created)
+        end
+    
+        # GET /todos/:id
+        def show
+            json_response(@todo)
+        end
+    
+        # PUT /todos/:id
+        def update
+            @todo.update(todo_params)
+            head :no_content
+        end
+    
+        # DELETE /todos/:id
+        def destroy
+            @todo.destroy
+            head :no_content
+        end
+    
+        private
+    
+        def todo_params
+            # whitelist params
+            # params.permit(:title, :created_by)
+            params.permit(:title)
+    
+        end
+    
+        def set_todo
+            @todo = Todo.find(params[:id])
+        end
+    
+    end
+end
